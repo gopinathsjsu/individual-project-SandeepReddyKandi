dbe38e1bd7b0639c457b5b13fb928da9686eac7b
SandeepReddyKandi <kandiSandeepReddy98@gmail.com>
2022-04-24 12:12:23 +0530

    add Main file and arguments error

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        if(args.length==0){
+            System.out.println("Please provide the file path to stocks inventory and cards as first and second arguments.");
+            System.exit(0);
+        }
+    }
+}
