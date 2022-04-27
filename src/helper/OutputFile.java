e746c4222f2d92cb07f28059ed8b0f0b1e4953a1
SandeepReddyKandi <kandiSandeepReddy98@gmail.com>
2022-04-27 17:21:23 +0530

    add helper files for logging

---
+++ src/helper/OutputFile.java
@@ -0,0 +1,10 @@
+package helper;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public interface OutputFile {
+    void writeToFile(ArrayList<String> content);
+    void save(Path path) throws IOException;
+}
